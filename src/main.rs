bc6427ac1d308bd8b462342a7db2d56d6474d6be
nick anderson <nick@morethq.com>
2026-07-10 23:20:55 -0700

    feat: initial cmd structure
    kupo stash open
    kupo stash close
    kupo help (fall-through)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
+use std::env;
+
 fn main() {
-    let name = "kupo";
-    println!("{} ✨", name);
+    let args: Vec<String> = env::args().collect();
+
+    match args.as_slice() {
+        [_, command, action] if command == "stash" => match parse_stash_cmd(action) {
+            Ok(cmd) => stash(cmd),
+            Err(err) => println!("{}", err),
+        },
+        _ => {
+            println!("usage: kupo stash open|close")
+        }
+    }
+}
+
+fn parse_stash_cmd(action: &str) -> Result<StashAction, String> {
+    match action {
+        "open" => Ok(StashAction::Open),
+        "close" => Ok(StashAction::Close),
+        _ => Err(format!("unknown action: {}", action)),
+    }
+}
+
+enum StashAction {
+    Open,
+    Close,
+}
+
+fn stash(action: StashAction) {
+    match action {
+        StashAction::Open => {
+            println!("opening stash, kupo!");
+        }
+        StashAction::Close => {
+            println!("closing stash, kupo!");
+        }
+    }
 }
