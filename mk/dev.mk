c4dd27d21c74c33966969b4b681bdf8315499772
nick anderson <nick@morethq.com>
2026-07-11 13:19:21 -0700

    feat: initial rust machinery via make

---
+++ mk/dev.mk
@@ -0,0 +1,34 @@
+.PHONY: check
+check: ## dev: do we compile?
+	@$(call log,"checking compilation")
+	@cargo check --all --tests --benches
+
+.PHONY: clean
+clean: ## dev: purge build artifacts
+	@$(call log,"cleaning build artifacts")
+	@cargo clean
+
+.PHONY: lint
+lint: ## dev: are we idiomatic?
+	@$(call log,"linting the workspace")
+	@cargo clippy --all-targets --all-features -- -D warnings
+
+.PHONY: format
+format: ## dev: format the code
+	@$(call log,"formatting the workspace")
+	@cargo fmt --verbose --all
+
+.PHONY: format-check
+format-check: ## dev: are we pretty?
+	@$(call log,"checking code formatting")
+	@cargo fmt --verbose --all -- --check
+
+.PHONY: test
+test: ## dev: do we profess to work?
+	@$(call log,"running all tests")
+	@cargo nextest run --workspace
+
+.PHONY: test-docs
+test-docs: ## dev: do our docs work?
+	@$(call log,"running all documentation tests")
+	@cargo test --doc --workspace
