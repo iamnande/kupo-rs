be0b34b29500e16809e55af3b78795e68c4e0b13
nick anderson <nick@morethq.com>
2026-07-11 12:36:54 -0700

    feat: result-driven command structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# kupo
+# kupo-rs
+
+`kupo` is a tiny Rust CLI that remembers where I last left my SD cards.
+
+`kupo stash open`, to open up shop and pilfer through our stash (photos/media).
+
+`kupo stash close`, to close up shop and relinquish our stash back to its
+liminal plane.
+
+a small offering to the great moogle archives, kupo!
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
-use std::env;
+use std::{env, error::Error, fmt};
+
+type Result<T> = std::result::Result<T, KupoError>;
+
+#[derive(Debug)]
+enum KupoError {
+    Usage,
+    UnknownStashAction(String),
+    Io(std::io::Error),
+}
+
+impl fmt::Display for KupoError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Usage => {
+                write!(f, "usage: kupo stash open|close")
+            }
+            Self::UnknownStashAction(action) => {
+                write!(f, "unknown stash action: {action}")
+            }
+            Self::Io(err) => {
+                write!(f, "{err}")
+            }
+        }
+    }
+}
+
+impl Error for KupoError {}
+
+impl From<std::io::Error> for KupoError {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
 
 enum KupoCommand {
     Stash(KupoStashAction),
@@ -9,39 +42,37 @@ enum KupoStashAction {
     Close,
 }
 
-fn main() {
-    match parse_args() {
-        Ok(command) => run(command),
-        Err(err) => eprintln!("{}", err),
+fn main() -> Result<()> {
+    let command = parse_args()?;
+    run(command)
+}
+
+fn run(command: KupoCommand) -> Result<()> {
+    match command {
+        KupoCommand::Stash(action) => stash(action),
     }
 }
 
-fn parse_args() -> Result<KupoCommand, String> {
+fn parse_args() -> Result<KupoCommand> {
     let args: Vec<String> = env::args().collect();
 
     match args.as_slice() {
         [_, command, action] if command == "stash" => {
             Ok(KupoCommand::Stash(parse_stash_action(action)?))
         }
-        _ => Err("usage: kupo stash open/close".into()),
+        _ => Err(KupoError::Usage),
     }
 }
 
-fn parse_stash_action(action: &str) -> Result<KupoStashAction, String> {
+fn parse_stash_action(action: &str) -> Result<KupoStashAction> {
     match action {
         "open" => Ok(KupoStashAction::Open),
         "close" => Ok(KupoStashAction::Close),
-        _ => Err(format!("unknown stash action: {}", action)),
+        _ => Err(KupoError::UnknownStashAction(action.to_owned())),
     }
 }
 
-fn run(command: KupoCommand) {
-    match command {
-        KupoCommand::Stash(action) => stash(action),
-    }
-}
-
-fn stash(action: KupoStashAction) {
+fn stash(action: KupoStashAction) -> Result<()> {
     match action {
         KupoStashAction::Open => {
             println!("opening stash, kupo!")
@@ -50,4 +81,6 @@ fn stash(action: KupoStashAction) {
             println!("closing stash, kupo!")
         }
     }
+
+    Ok(())
 }
